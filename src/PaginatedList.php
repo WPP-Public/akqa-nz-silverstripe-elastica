bd5749effcc8987a91f04427342d72b62f806481
Glenn Bautista <glenn@vinta.tech>
2018-01-17 14:49:47 +0800

    Added custom Paginated List to make pagination easier

---
+++ src/PaginatedList.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Heyday\Elastica;
+
+
+/**
+ * Class PaginatedList
+ * @package Heyday\Elastica
+ */
+class PaginatedList extends \SilverStripe\ORM\PaginatedList
+{
+    /**
+     * Use the ResultList's total items method to determine this value
+     *
+     * @return int
+     */
+    public function getTotalItems()
+    {
+        if ($this->list instanceof ResultList) {
+            return $this->list->totalItems();
+        }
+
+        return parent::getTotalItems();
+    }
+}
