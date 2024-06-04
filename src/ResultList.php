6fd2aab4a8bddb24faef04524c795242177d6ddc
Will Rossiter <will@fullscreen.io>
2024-06-04 15:49:49 +1200

    fix: remove debugging

--- src/ResultList.php
+++ src/ResultList.php
@@ -297,7 +297,6 @@ class ResultList extends ViewableData implements SS_List, Limitable
         try {
             $from = $this->getQuery()->getParam('from');
         } catch (Exception $e) {
-            dd($e);
             $from = 1;
         }
 
